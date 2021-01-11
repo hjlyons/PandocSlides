45da0191c3b32aac62abe44c38b769f81f5ef95a
Harry John Lyons <hlyons1994@gmail.com>
2021-01-11 10:22:46 +0000

    only open preview on successful build

--- compile.sh
+++ compile.sh
@@ -2,11 +2,9 @@
 
 DATE=$(date '+%Y_%m_%d');
 TAG=PRESENTATION
-#title="Plots for Harry"
-#author="Michael"
-#date="13th October 2020"
-#sed -i "s/title:.*/title: ${title}/;s/author:.*/author: ${author}/;s/date:.*/date: ${date}/" markdown_beamer_example.md
 
-pandoc markdown_beamer_example.md -t beamer -o ${DATE}_${TAG}.pdf --slide-level 2 --include-in-header style/my_style.tex
+if pandoc markdown_beamer_example.md -t beamer -o ${DATE}_${TAG}.pdf --slide-level 2 --include-in-header style/my_style.tex --toc ; then
+    open .
+fi
 
-open ${DATE}_${TAG}.pdf
+#open ${DATE}_${TAG}.pdf
